4495c62abf93ecf31757e5d728cabcb5872b3a68
Nahom <getiyenahom751@gmail.com>
2025-02-02 06:55:42 +0300

    edit test post

---
+++ app/testPosts/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import prisma from '@/lib/db';
+
+const TestPage = async () => {
+  const postts = await prisma.post.findMany();
+
+  return (
+    <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
+      <h1 className="text-3xl font-semibold">All Posts {postts.length}</h1>
+
+      <ul className="grid grid-cols-1 gap-y-4">
+        {postts.map((post) => (
+          <li key={post.id} className="flex flex-col items-start p-4 border rounded-lg shadow">
+            <h2 className="text-xl font-bold">{post.title}</h2>
+            <p className="text-base text-gray-700">{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default TestPage;
